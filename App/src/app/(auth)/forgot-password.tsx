b871fc8388d1cc1b97da109c6cee5714fd934e96
himanshur-trav <himanshu.r@travsetgo.com>
2026-09-18 18:08:07 +0530

    Added username,register,forgot screens

--- App/src/app/(auth)/forgot-password.tsx
+++ App/src/app/(auth)/forgot-password.tsx
@@ -1,11 +1,109 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import { AuthLayout } from "@/components/auth/AuthLayout";
+import { LinearGradient } from "expo-linear-gradient";
+import { globalColors, globalStyles } from "@/styles/themes";
+import loginStyle from "./styles/styles.login";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import forgotStyle from "./styles/styles.forgot";
+import useAuthForgotStore from "./store/useAuth.forgot";
+import RegisterStyle from "./styles/styles.register";
+import { Link } from "expo-router";
 
 const ForgotPasswordScreen = () => {
+  const [input, setInput] = useState({
+    email: "",
+  });
+  const { isLoading } = useAuthForgotStore();
+
+  const handleForgot = () => {};
+
   return (
-    <View>
-      <Text>forgotPasswordScreen</Text>
-    </View>
+    <AuthLayout>
+      <LinearGradient
+        colors={globalColors.gradientLight.color}
+        locations={globalColors.gradientLight.location}
+        style={[loginStyle.formContainer, loginStyle.keyboardLayoutContainer]}
+      >
+        <View style={forgotStyle.forgotextContainer}>
+          <Text style={forgotStyle.Heading}>Forgot Password</Text>
+
+          <Text style={forgotStyle.inputfieldText}>
+            Enter your email address and we'll send you a link to reset your
+            password.
+          </Text>
+        </View>
+
+        <View style={[loginStyle.inputFieldContainer]}>
+          <Text style={[loginStyle.inputFieldLabel]}>Email</Text>
+          <View style={[loginStyle.inputWrapper]}>
+            <MaterialCommunityIcons
+              name="email-outline"
+              size={20}
+              color={globalColors.primaryColor}
+              style={[loginStyle.frontIcons]}
+            />
+            <TextInput
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              autoComplete="email"
+              textContentType="emailAddress"
+              placeholderTextColor={globalColors.grayDark_50}
+              accessible={true}
+              accessibilityLabel="Email input field"
+              accessibilityHint="Enter your email id"
+              accessibilityRole="text"
+              returnKeyType="send"
+              blurOnSubmit={false}
+              value={input.email}
+              onChangeText={(text) =>
+                setInput((prev) => ({ ...prev, email: text }))
+              }
+              style={[globalStyles.input_field]}
+            />
+          </View>
+        </View>
+
+        <TouchableOpacity
+          onPress={handleForgot}
+          style={[globalStyles.primary_btn, RegisterStyle.btnContainer]}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <>
+              <Text style={[loginStyle.btnText]}>Sending OTP</Text>
+              <Ionicons
+                name="reload-outline"
+                size={20}
+                color={globalColors.backgroundLight}
+              />
+            </>
+          ) : (
+            <>
+              <Text style={[loginStyle.btnText]}>Send OTP</Text>
+              <Ionicons
+                name="arrow-forward"
+                size={20}
+                color={globalColors.backgroundLight}
+              />
+            </>
+          )}
+        </TouchableOpacity>
+
+        <TouchableOpacity style={forgotStyle.backToLoginLinkContainer}>
+          <Link href="/login" style={forgotStyle.backToLoginLink}>
+            <Ionicons
+              name="arrow-back"
+              size={20}
+              color={globalColors.primaryColor}
+            />
+            <Text style={forgotStyle.backToLoginLinkText}>Back to Login</Text>
+          </Link>
+        </TouchableOpacity>
+      </LinearGradient>
+    </AuthLayout>
   );
 };
 
--- App/src/app/(auth)/styles/styles.register.ts
+++ App/src/app/(auth)/styles/styles.register.ts
@@ -1,5 +1,23 @@
+import { globalColors, globalStyles, globalValues } from "@/styles/themes";
+import { responsiveHeight, responsiveWidth } from "@/utilities/responsive";
 import { StyleSheet } from "react-native";
 
-const RegisterStyle = StyleSheet.create({});
+const RegisterStyle = StyleSheet.create({
+  btnContainer: {
+    marginTop: responsiveHeight(15),
+  },
+  btnText: {
+    fontSize: globalValues.h4,
+    color: globalColors.backgroundLight,
+  },
+  TermsContainer: {
+    paddingVertical: responsiveHeight(5),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  TermsText: {
+    color: globalColors.grayDark_50,
+  },
+});
 
 export default RegisterStyle;
